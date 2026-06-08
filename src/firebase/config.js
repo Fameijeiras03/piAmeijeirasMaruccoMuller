76ef39f69678fc949a92f6c25e4a30075703cc60
fameijeiras03 <fameijeiras1@gmail.com>
2026-06-08 12:23:07 -0300

    inicializando el proyecto RN

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAPbPk7jUTAtEiQXROAeR4qEr5HjUEy040",
+    authDomain: "pi-a-m-m.firebaseapp.com",
+    projectId: "pi-a-m-m",
+    storageBucket: "pi-a-m-m.firebasestorage.app",
+    messagingSenderId: "719276315848",
+    appId: "1:719276315848:web:9b8f2bd49c9965523881fd"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = app.firestore();
